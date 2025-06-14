c0b3e1568c2d5850f151a5da8cce2eaa39b976d3
Fikri Ajiwijaya <fikri.ajiwijaya@gmail.com>
2025-06-14 06:17:55 +0000

    Solve problem 0003

--- problem_0003/problem_0003.go
+++ problem_0003/problem_0003.go
@@ -5,5 +5,19 @@ func LengthOfLongestSubstring(s string) int {
 }
 
 func lengthOfLongestSubstring(s string) int {
-	return 0
+	max_length := 0
+	pos := map[rune]int{}
+	beg := 0
+	for i, r := range s {
+		loc, found := pos[r]
+		if found && loc >= beg {
+			beg = loc + 1
+		}
+		pos[r] = i
+		length := i + 1 - beg
+		if length > max_length {
+			max_length = length
+		}
+	}
+	return max_length
 }
