84195ae5850479dfc64f112d55dbda96e0643243
Fikri Ajiwijaya <fikri.ajiwijaya@gmail.com>
2025-06-14 12:53:43 +0000

    Create test for problem 0004

--- problem_0004/problem_0004.go
+++ problem_0004/problem_0004.go
@@ -1,5 +1,9 @@
 package problem_0004
 
+func FindMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	return findMedianSortedArrays(nums1, nums2)
+}
+
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	return 0
 }
---
+++ problem_0004/problem_0004_test.go
@@ -0,0 +1,26 @@
+package problem_0004_test
+
+import (
+	"testing"
+
+	. "github.com/fikri-ajiwijaya/leetcode.go/problem_0004"
+)
+
+func FindMedianSortedArrays_test(t *testing.T, nums1 []int, nums2 []int, expected float64) {
+	result := FindMedianSortedArrays(nums1, nums2)
+	if result != expected {
+		t.Errorf("nums1 = %#v", nums1)
+		t.Errorf("nums2 = %#v", nums2)
+		t.Errorf("expected = %#v", expected)
+		t.Errorf("result = FindMedianSortedArrays(nums1, nums2)")
+		t.Errorf("result is %#v", result)
+	}
+}
+
+func TestExample1(t *testing.T) {
+	FindMedianSortedArrays_test(t, []int{1, 3}, []int{2}, 2.0)
+}
+
+func TestExample2(t *testing.T) {
+	FindMedianSortedArrays_test(t, []int{1, 2}, []int{3, 4}, 2.5)
+}
