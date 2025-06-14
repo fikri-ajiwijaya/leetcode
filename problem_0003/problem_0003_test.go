ec75897842dbaa376ef561262c1d0b5b43b5bdbc
Fikri Ajiwijaya <fikri.ajiwijaya@gmail.com>
2025-06-14 06:17:14 +0000

    Create test for problem 0003

---
+++ problem_0003/problem_0003_test.go
@@ -0,0 +1,38 @@
+package problem_0003_test
+
+import (
+	"testing"
+
+	. "github.com/fikri-ajiwijaya/leetcode.go/problem_0003"
+)
+
+func LengthOfLongestSubstring_test(t *testing.T, s string, expected int) {
+	result := LengthOfLongestSubstring(s)
+	if expected != result {
+		t.Errorf("s = %#v", s)
+		t.Errorf("expected = %#v", expected)
+		t.Errorf("result = LengthOfLongestSubstring(s)")
+		t.Errorf("result is %#v", result)
+	}
+}
+
+func TestExample1(t *testing.T) {
+	LengthOfLongestSubstring_test(t, "abcabcbb", 3)
+}
+
+func TestExample2(t *testing.T) {
+	LengthOfLongestSubstring_test(t, "bbbbb", 1)
+}
+
+func TestExample3(t *testing.T) {
+	LengthOfLongestSubstring_test(t, "pwwkew", 3)
+}
+func TestLengthOfLongestSubstringv4(t *testing.T) {
+	LengthOfLongestSubstring_test(t, "", 0)
+}
+func TestLengthOfLongestSubstringv5(t *testing.T) {
+	LengthOfLongestSubstring_test(t, " ", 1)
+}
+func TestLengthOfLongestSubstringv6(t *testing.T) {
+	LengthOfLongestSubstring_test(t, "abcaaaaaaaaa", 3)
+}
