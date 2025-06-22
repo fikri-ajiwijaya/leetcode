86f3c41e72a3e3cf49da300936ef8f08fd0ac55e
Fikri Ajiwijaya <fikri.ajiwijaya@gmail.com>
2025-06-22 15:00:24 +0000

    Solve problem 0005

--- problem_0005/problem_0005.go
+++ problem_0005/problem_0005.go
@@ -4,6 +4,46 @@ func LongestPalindrome(s string) string {
 	return longestPalindrome(s)
 }
 
+func get_palindrome_length_at(r []rune, i int) int {
+	length_odd := 1
+	for j, k := i-1, i+1; j >= 0 && k < len(r); j, k = j-1, k+1 {
+		if r[j] != r[k] {
+			break
+		}
+		length_odd += 2
+	}
+	length_even := 0
+	for j, k := i-1, i; j >= 0 && k < len(r); j, k = j-1, k+1 {
+		if r[j] != r[k] {
+			break
+		}
+		length_even += 2
+	}
+	if length_odd > length_even {
+		return length_odd
+	} else {
+		return length_even
+	}
+}
+
+func get_palindrome_at(r []rune, i int, length int) string {
+	if length%2 == 0 {
+		return string(r[i-length/2 : i+length/2])
+	} else {
+		return string(r[i-length/2 : i+length/2+1])
+	}
+}
+
 func longestPalindrome(s string) string {
-	return ""
+	longest := 0
+	pos := -1
+	r := []rune(s)
+	for i := 0; i < len(r); i++ {
+		length := get_palindrome_length_at(r, i)
+		if length > longest {
+			longest = length
+			pos = i
+		}
+	}
+	return get_palindrome_at(r, pos, longest)
 }
--- problem_0005/problem_0005_test.go
+++ problem_0005/problem_0005_test.go
@@ -1,14 +1,15 @@
 package problem_0005_test
 
 import (
+	"slices"
 	"testing"
 
 	. "github.com/fikri-ajiwijaya/leetcode.go/problem_0005"
 )
 
-func LongestPalindrome_test(t *testing.T, s string, expected string) {
+func LongestPalindrome_test(t *testing.T, s string, expected []string) {
 	result := LongestPalindrome(s)
-	if result != expected {
+	if !slices.Contains(expected, result) {
 		t.Errorf("s = %#v", s)
 		t.Errorf("expected = %#v", expected)
 		t.Errorf("result = LongestPalindrome(s)")
@@ -17,9 +18,17 @@ func LongestPalindrome_test(t *testing.T, s string, expected string) {
 }
 
 func TestExample1(t *testing.T) {
-	LongestPalindrome_test(t, "babad", "bab")
+	LongestPalindrome_test(t, "babad", []string{"bab", "aba"})
 }
 
 func TestExample2(t *testing.T) {
-	LongestPalindrome_test(t, "cbbd", "bb")
+	LongestPalindrome_test(t, "cbbd", []string{"bb"})
+}
+
+func TestExample3(t *testing.T) {
+	LongestPalindrome_test(t, "bb", []string{"bb"})
+}
+
+func TestExample4(t *testing.T) {
+	LongestPalindrome_test(t, "aacabdkacaa", []string{"aca"})
 }
