4f5a5d8821005b4f4040a00e5e3c87ada7889970
Fikri Ajiwijaya <fikri.ajiwijaya@gmail.com>
2025-06-23 06:01:33 +0000

    Solve problem 0006

--- problem_0006/problem_0006.go
+++ problem_0006/problem_0006.go
@@ -5,5 +5,30 @@ func Convert(s string, numRows int) string {
 }
 
 func convert(s string, numRows int) string {
-	return ""
+	if numRows < 1 {
+		return ""
+	}
+	if numRows == 1 {
+		return s
+	}
+	r := []rune(s)
+	new := make([]rune, 0, len(r))
+	for i := 0; i < numRows; i++ {
+		for j := 0; ; j++ {
+			{
+				k := j*(2*numRows-2) + i
+				if k >= len(r) {
+					break
+				}
+				new = append(new, r[k])
+			}
+			if i > 0 && i < numRows-1 {
+				k := (j+1)*(2*numRows-2) - i
+				if k < len(r) {
+					new = append(new, r[k])
+				}
+			}
+		}
+	}
+	return string(new)
 }
