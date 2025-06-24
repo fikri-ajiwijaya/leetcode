29b1f6e7578ba13cdb42679ea4f8d9f61d0487e2
Fikri Ajiwijaya <fikri.ajiwijaya@gmail.com>
2025-06-24 06:49:52 +0000

    Solve problem 0007

--- problem_0007/problem_0007.go
+++ problem_0007/problem_0007.go
@@ -5,5 +5,21 @@ func Reverse(x int) int {
 }
 
 func reverse(x int) int {
-	return 0
+	if x == 0 {
+		return 0
+	}
+	for x%10 == 0 {
+		x /= 10
+	}
+	y := 0
+	for x != 0 {
+		if y >= 214748364 || y <= -214748364 {
+			if !((y == 214748364 && x%10 <= 7) || (y == -214748364 && x%10 >= -8)) {
+				return 0
+			}
+		}
+		y = y*10 + x%10
+		x = x / 10
+	}
+	return y
 }
--- problem_0007/problem_0007_test.go
+++ problem_0007/problem_0007_test.go
@@ -27,3 +27,27 @@ func TestExample2(t *testing.T) {
 func TestExample3(t *testing.T) {
 	Reverse_test(t, 120, 21)
 }
+
+func TestExample4(t *testing.T) {
+	Reverse_test(t, 1534236469, 0)
+}
+
+func TestExample5(t *testing.T) {
+	Reverse_test(t, 0, 0)
+}
+
+func TestExample6(t *testing.T) {
+	Reverse_test(t, 7463847412, 2147483647)
+}
+
+func TestExample7(t *testing.T) {
+	Reverse_test(t, 8463847412, 0)
+}
+
+func TestExample8(t *testing.T) {
+	Reverse_test(t, -8463847412, -2147483648)
+}
+
+func TestExample9(t *testing.T) {
+	Reverse_test(t, -9463847412, 0)
+}
